b7cd1113a6f217a09278fbde0e43df618677d242
Bryan Fox <git@bryfox.com>
2021-09-22 16:15:43 -0400

    Update import task for Ecto 3+

--- lib/mix/tasks/fairbanks_data_import.ex
+++ lib/mix/tasks/fairbanks_data_import.ex
@@ -1,11 +1,27 @@
 defmodule Mix.Tasks.Fairbanks.Data.Import do
   use Mix.Task
-  import Mix.Ecto
 
   @shortdoc "Download remote data, save to DB, and exit"
   def run(_) do
-    Application.ensure_all_started(:httpoison)
-    Mix.Task.run("app.start")
+    start_repo()
     Fairbanks.Importing.Coordinator.import_once()
+    stop_repo()
+  end
+
+  defp start_repo do
+    Enum.each(
+      [
+        :postgrex,
+        :ecto,
+        :ecto_sql,
+        :httpoison
+      ],
+      &Application.ensure_all_started/1
+    )
+    Fairbanks.Repo.start_link()
+  end
+
+  defp stop_repo do
+    :init.stop()
   end
 end
